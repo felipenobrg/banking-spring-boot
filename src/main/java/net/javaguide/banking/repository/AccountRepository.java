c38516bdb6ddd2737b23517261b6652011c74009
felipenobrg <felipenobrega2012@gmail.com>
2025-02-13 20:36:59 -0300

    add repository

---
+++ src/main/java/net/javaguide/banking/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package net.javaguide.banking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.javaguide.banking.entity.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
