5ec2178a1fa67ccb13b9b12b068d45a34401fdbc
felipenobrg <felipenobrega2012@gmail.com>
2025-02-13 20:19:36 -0300

    create account entity

--- src/main/java/net/javaguide/banking/entity/Account.java
+++ src/main/java/net/javaguide/banking/entity/Account.java
@@ -1,7 +1,28 @@
 package net.javaguide.banking.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter()
+@Getter()
+@NoArgsConstructor()
+@AllArgsConstructor()
+@Table(name = "accounts")
+@Entity()
 public class Account {
+    @Id()
+    @GeneratedValue()
     private Long id;
+
+    @Column(name = "account_holder_name")
     private String accountHolderName;
+    private Double balance;
 
 }
