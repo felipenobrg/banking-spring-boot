37ac36000fa0cfc17c055cc43920e57576797b4d
felipenobrg <felipenobrega2012@gmail.com>
2025-02-27 08:57:15 -0300

    improve login validation

--- src/main/java/net/javaguide/banking/dto/LoginRequest.java
+++ src/main/java/net/javaguide/banking/dto/LoginRequest.java
@@ -1,12 +1,24 @@
 package net.javaguide.banking.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginRequest {
+
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
     private String password;
 }
